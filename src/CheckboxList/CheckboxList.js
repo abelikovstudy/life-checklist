d09590713bac6aca76c05a31fd37f6b855e5e831
Belikov A <beli@sfedu.ru>
2023-09-03 02:11:44 +0300

    two states replaced with one object state

--- src/CheckboxList/CheckboxList.js
+++ src/CheckboxList/CheckboxList.js
@@ -3,20 +3,30 @@ import React, { useEffect, useState } from "react"
 export default function CheckboxList(props){
     const title = props.title
     const length = props.content.length
-    const [checkedList, setCheckedList] = useState(Array(length).fill(false))
-    const [totalCounter, setTotalCounter] = useState(0)
+    const [listStatus, setListStatus] = useState({
+        checkedList : Array(length).fill(false),
+        totalCount : 0
+    })
     const content = props.content.map(
         (element,i) => 
         <li className="list-element" key={i}>
-            <button className="list-element-content" style={{ textDecoration: checkedList[i] ? "line-through" : "none"}} onClick={() => handleClick(i)}>{element}</button>
+            <button className="list-element-content" style={{ textDecoration: listStatus.checkedList[i] ? "line-through" : "none"}} onClick={() => handleClick(i)}>{element}</button>
         </li>
     )
     const handleClick = (index) => {
-        setCheckedList(checkedList.map((element, i) => (i === index) ? !element : element))
+        setListStatus({
+            ...listStatus,
+            checkedList : listStatus.checkedList.map((element, i) => (i === index) ? !element : element),
+        })
     }
+
+
     useEffect(() => {
-        setTotalCounter(checkedList.filter((element) => element).length)
-    },[checkedList,totalCounter])
+        setListStatus((prev) => ({
+            ...prev,
+            totalCount : listStatus.checkedList.filter((element) => element).length
+        }))
+    },[listStatus.checkedList,listStatus.totalCount])
     return(
         <div className="list-box">
             <h1 className="list-title">
@@ -25,7 +35,7 @@ export default function CheckboxList(props){
             <ul className="list-holder">
                 {content}
             </ul>
-            <progress value={totalCounter} max={length}/>
+            <progress value={listStatus.totalCount} max={length}/>
         </div>
     )
 }
